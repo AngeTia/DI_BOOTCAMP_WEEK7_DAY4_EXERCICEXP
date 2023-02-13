e0717bd23de75aa0489a2271a4cdb6a98ef890d8
AngeTia <tiaange03@gmail.com>
2023-02-13 19:10:05 +0000

    Update

--- Exercice_4/tests/ExampleTest.php
+++ Exercice_4/tests/ExampleTest.php
@@ -6,15 +6,20 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
    
+    public $isTrue = true;
     /**
      * @test
      */
     public function phpunit_works(): void
     {
-        // self::assertTrue(true);
-        $this->assertCount(2, [1, 2]);
-        // $this->assertIsInt('12');
-
+        // self::assertTrue($this->isTrue);
+        // $a = "la";
+        // $b = "le";
+        // $c = $a + $b;
+        // echo $c;
+        // die;
+        // $this->assertIsInt($c);
+        $this->assertIsArray(5);
 
     }
 }
